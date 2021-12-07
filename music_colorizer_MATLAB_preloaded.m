40d2c97d6f49ee1d1ff851c2e9133dfc32dd3224
Megan Ku <mku@olin.edu>
2021-12-07 13:49:17 -0500

    Add documentation to code

--- music_colorizer_MATLAB_preloaded.m
+++ music_colorizer_MATLAB_preloaded.m
@@ -11,16 +11,28 @@ MATLAB Support Package for Arduino Hardware
 Neopixel Add-On Library for MATLAB
 %}
 
+%INITIALIZE ARDUINO
+COM = 'COM3';
+ARDUINO_TYPE = 'Mega2560';
+DIG_PIN = "D53";
+
+NUM_LED = 120;
+
 % Set constants
 Fs = 44100; % Sampling frequency
 
 max_brightness = 0.8; % Max Neopixel brightness (scale of 0 to 1)
-num_LEDs = 120;
-LED_range = 1:num_LEDs;
+
+LED_range = 1:NUM_LED;
 
 % Control how many samples go into FFT per cycle - adjust number to account
 % for lag in processing FFT and changing lights
-FFT_samp = Fs * 0.08; 
+% For in-the-moment fine tuning, change FFT_samp to equal Fs * approximate
+% value of tic / toc output within loop to minimize delay
+% UNCOMMENT LINE 31 IF NOT PLOTTING
+% FFT_samp = round(Fs * 0.08);
+% UNCOMMENT IF PLOTTING
+FFT_samp = round(Fs * 0.16);
 
 % Bandpass filter cutoffs
 
@@ -28,58 +40,76 @@ FREQ_LOW = [129, 265];
 FREQ_MID = [247, 523.4];
 FREQ_HIGH = [523, 1047];
 
-% Initialize Arduino Mega 2560 - adjust port number as necessary
-a = arduino('COM8', 'Mega2560', 'Libraries', 'Adafruit/NeoPixel');
-neostrip = addon(a, 'Adafruit/NeoPixel', 'D53', 120);
+% Initialize Arduino Mega 2560
+a = arduino(COM, ARDUINO_TYPE, 'Libraries', 'Adafruit/NeoPixel');
+neostrip = addon(a, 'Adafruit/NeoPixel', DIG_PIN, NUM_LED);
 
 % Play stored MP3 file
-[x_full, Fs] = audioread("data/bensound-sunny.mp3");
+[x_full, Fs] = audioread("data/bensound-sunny.mp3", [1 20*Fs]);
 soundsc(x_full, Fs);
 
+% Run loop for duration of played sound
 for i = 1:(floor(length(x_full) / FFT_samp)-1)
-
+    tic % uncomment to test timing
+    % Select time period of audio file to run FFT
     start = i * FFT_samp - (FFT_samp - 1);
     finish = i * FFT_samp + FFT_samp;
     x = x_full(start:finish, 1);
     
+    % Run FFT and take magnitude
     y = abs(fftshift(fft(x)));
     y = y(:,1);
     f = linspace(-Fs/2, Fs/2*(length(y)-1)/length(y), length(y))';
     y_full = [f y];
     
-    LED_set = zeros(num_LEDs, 3);
-
+    % Initialize matrix to store LED data
+    LED_set = zeros(NUM_LED, 3);
+    
+    % Idealized bandpass filters
+    % Low (RED)
     l = (FREQ_LOW(1) < f) & (f < FREQ_LOW(2));
     y_low = y_full(l,:);
     y_low = imresize(y_low, [120, 2]);
     
+    % Mid (GREEN)
     m = (FREQ_MID(1) < f) & (f < FREQ_MID(2));
     y_mid = y_full(m,:);
     y_mid = imresize(y_mid, [120, 2]);
     
+    % High (BLUE)
     h = (FREQ_HIGH(1) < f) & (f < FREQ_HIGH(2));
     y_high = y_full(h,:);
     y_high = imresize(y_high, [120, 2]);
-    
-    LED_set(:, 1) = y_low(:,2) / max(y_low(:, 2));
-    LED_set(:, 2) = y_mid(:,2) / max(y_mid(:, 2));
-    LED_set(:, 3) = y_high(:,2) / max(y_high(:, 2));
     
     LED_set(:, 1) = y_low(:,2);
     LED_set(:, 2) = y_mid(:,2);
     LED_set(:, 3) = y_high(:,2);
     
+    % Normalization
     LED_set = abs(LED_set)./ max(max(LED_set));
     
+    % Adjust for max_brightness
     LED_set = LED_set .* max_brightness;
     
-    figure(2);
+    % COMMENT OUT BOTH FIGURES IF NOT PLOTTING
+    figure(1);
     plot(y_low(:,1), y_low(:,2), "r", y_mid(:,1), y_mid(:,2), "g", y_high(:,1), y_high(:,2), "b");
-    figure(3);
+    ylim([0 600]);
+    xlabel("Frequency (Hz)");
+    ylabel("Magnitude");
+    title("Frequency Response of Sample Tune");
+       
+    figure(2);
     plot(LED_range, LED_set(:,1), "r", LED_range, LED_set(:,2), "g", LED_range, LED_set(:,3), "b");
+    ylim([0,0.8]);
+    xlabel("LED Number");
+    ylabel("LED Brightness");
+    title("LED Color Assignment for Sample Tune");
 
+    % Write color to LEDs
     writeColor(neostrip, LED_range, LED_set);
-   
+    toc % uncomment to test timing
 end
 
-writeColor(neostrip, LED_range, zeros(num_LEDs, 3));
+% Clear strip after song ends
+writeColor(neostrip, LED_range, zeros(NUM_LED, 3));
